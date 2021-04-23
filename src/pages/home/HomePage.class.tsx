90a27c1f7a84df582498fbce59d80dc06fe63dad
taotao_glb <taotao_glb@icloud.com>
2021-04-23 16:18:55 +0800

    fetch data for HomePage use axios

--- src/pages/home/HomePage.class.tsx
+++ src/pages/home/HomePage.class.tsx
@@ -6,18 +6,62 @@ import {
 	SideMenu,
 	ProductCollection,
 } from '../../components'
-import { Row, Col, Typography } from 'antd'
-import { productList1, productList2, productList3 } from './mockups'
+import { Row, Col, Typography, Spin } from 'antd'
 import sideImage from '../../assets/images/sider_2019_12-09.png'
 import sideImage2 from '../../assets/images/sider_2019_02-04.png'
 import sideImage3 from '../../assets/images/sider_2019_02-04-2.png'
+import { productList1, productList2, productList3 } from './mockups'
 import styles from './HomePage.module.css'
 import { withTranslation, WithTranslation } from 'react-i18next'
+import axios from 'axios'
 
-class HomePageComponent extends React.Component<WithTranslation> {
+interface State {
+	loading: boolean
+	error: string | null
+	productList: any[]
+}
+
+class HomePageComponent extends React.Component<WithTranslation, State> {
+	constructor(props) {
+		super(props)
+		this.state = {
+			loading: true,
+			error: null,
+			productList: [],
+		}
+	}
+	async componentDidMount() {
+		try {
+			const { data } = await axios.get(
+				'https://jsonplaceholder.cypress.io/posts',
+			)
+			const dataList = data.splice(0, 9)
+			this.setState({ productList: dataList, loading: false, error: null })
+		} catch (error) {
+			this.setState({ error: error, loading: false })
+		}
+	}
+
 	render() {
-		console.log(this.props)
 		const { t } = this.props
+		const { error, loading } = this.state
+		if (loading) {
+			return (
+				<Spin
+					size="large"
+					style={{
+						marginTop: 200,
+						marginBottom: 200,
+						marginLeft: 'auto',
+						marginRight: 'auto',
+						width: '100%',
+					}}
+				/>
+			)
+		}
+		if (error) {
+			return <div>网站错误：{error}</div>
+		}
 		return (
 			<>
 				<Header />
